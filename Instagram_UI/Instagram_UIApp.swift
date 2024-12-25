3f3eba57b9e01410b117b6bac11382f3796085fc
jothiredblox <jothimariyappan.s@redblox.io>
2024-12-25 19:43:13 +0530

    Initial Commit

---
+++ Instagram_UI/Instagram_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_UIApp.swift
+//  Instagram_UI
+//
+//  Created by jo on 25/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
